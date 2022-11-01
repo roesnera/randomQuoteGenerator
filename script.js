24ca92a6e015b218479f04f46fcd028f1cacf5ad
roesnera <adam.roesner@generaiton.org>
2022-11-01 09:21:11 -0400

    memoizes the getQuote function to avoid repeated, unnecessary fetch calls

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+const memoizedGetQuote = () => {
+    let quotesCache;
+    return async () => {
+        try {
+            const auth = document.getElementById('author');
+            const quote = document.getElementById('quote');
+            if(!quotesCache){
+                console.log('making fetch call')
+                let quoteArray = await fetch('https://type.fit/api/quotes');
+                quotesCache = await quoteArray.json();
+            } else {
+                console.log('using cached quote')
+            }
+            const randNum = Math.floor(Math.random() *quotesCache.length);
+            const randQuote = quotesCache[randNum];
+            quote.innerText = randQuote.text;
+            auth.innerText = randQuote.author;
+        } catch(err) {
+            console.log(err.message);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 async function getQuote() {
     try {
         const auth = document.getElementById('author');
@@ -14,5 +48,5 @@ async function getQuote() {
 }
 
 const btn = document.getElementById("getQuote");
-btn.addEventListener("click", getQuote);
-onload = getQuote;
+btn.addEventListener("click", memoizedGetQuote());
+onload = memoizedGetQuote();
