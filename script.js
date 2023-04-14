6b3921a52ae3f32e005b5584ea8280f4dc23def6
roesnera <adam.roesner@generation.org>
2023-04-14 12:50:45 -0400

    modifies original solution to avoid async js

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
-// fetch the resource using fetch()
-async function fetchResource() {
-    try {
-        let url = 'https://type.fit/api/quotes';
-        let resource = await fetch(url);
-        // parse the resource into an object
-        let data = await resource.json();
-        // grab a random entry from the returned object
-        
-        // generate a random number between 0 and the length of my arrays
-        let randomIndex = Math.floor(Math.random() * data.length);
-        // select the element corresponding to that random number
-        let element = data[randomIndex];
-        // update the index.html page with the randomly selected entry
-        const quoteP = document.getElementById('quote');
-        const authH3 = document.querySelector('#author');
-        quoteP.innerHTML = element.text;
-        authH3.innerText = element.author;
-    } catch(robert) {
-        console.error(robert);
-    }
+import quotes from './dummyAPI.js';
+
+
+function displayRandomQuote() {
+    // generate a random number between 0 and the length of my arrays
+    let randomIndex = Math.floor(Math.random() * quotes.length);
+    // select the element corresponding to that random number
+    let element = quotes[randomIndex];
+    // update the index.html page with the randomly selected entry
+    const quoteP = document.getElementById('quote');
+    const authH3 = document.querySelector('#author');
+    quoteP.innerHTML = element.text;
+    authH3.innerText = element.author;
 }
 
+
 let myButton = document.getElementById('getQuote');
-myButton.onclick = fetchResource;
+myButton.onclick = displayRandomQuote;
