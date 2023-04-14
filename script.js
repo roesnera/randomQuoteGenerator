c41c165a34c57ce28cbf559824046f8180687bed
roesnera <adam.roesner@generation.org>
2023-04-14 12:47:42 -0400

    edits to script.js

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-async function getQuote() {
+// fetch the resource using fetch()
+async function fetchResource() {
     try {
-        const auth = document.getElementById('author');
-        const quote = document.getElementById('quote');
-        let quoteArray = await fetch('https://type.fit/api/quotes');
-        quoteArray = await quoteArray.json();
-        const randNum = Math.floor(Math.random() *quoteArray.length);
-        const randQuote = quoteArray[randNum];
-        quote.innerText = randQuote.text;
-        auth.innerText = randQuote.author;
-    } catch(err) {
-        console.log(err.message);
+        let url = 'https://type.fit/api/quotes';
+        let resource = await fetch(url);
+        // parse the resource into an object
+        let data = await resource.json();
+        // grab a random entry from the returned object
+        
+        // generate a random number between 0 and the length of my arrays
+        let randomIndex = Math.floor(Math.random() * data.length);
+        // select the element corresponding to that random number
+        let element = data[randomIndex];
+        // update the index.html page with the randomly selected entry
+        const quoteP = document.getElementById('quote');
+        const authH3 = document.querySelector('#author');
+        quoteP.innerHTML = element.text;
+        authH3.innerText = element.author;
+    } catch(robert) {
+        console.error(robert);
     }
 }
 
-const btn = document.getElementById("getQuote");
-btn.addEventListener("click", getQuote);
-onload = getQuote;
+let myButton = document.getElementById('getQuote');
+myButton.onclick = fetchResource;
